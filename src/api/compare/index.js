023c4b90d12ad49ad06fca8050ff090d24462b97
Flor Kaucic <florkaucic@gmail.com>
2018-02-07 15:45:51 -0300

    Password and hash comparison endpoint finished

--- src/api/compare/index.js
+++ src/api/compare/index.js
@@ -1,15 +1,18 @@
 const { Router } = require("express");
 
 const Logger = require("../../logger");
+const { compareHashWithValue } = require("../../utils");
 
 const router = new Router();
 
-router.get("/password/compare", (req, res) => {
+router.get("/password/compare", async (req, res) => {
     Logger.log('> Controller - Password comparison');
+
+    const comparisonResult = await compareHashWithValue(req.query.hash, req.query.value);
 
     res.status(200);
     res.setHeader("content-type", "application/json; charset=utf-8");
-    res.json({ result: "true" });
+    res.json({ result: comparisonResult });
 });
 
 module.exports = router;
