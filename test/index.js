576096186478a921a0df0b0219c369d6c2190426
Flor Kaucic <florkaucic@gmail.com>
2018-02-08 21:14:53 -0300

    Missing tests for verify, compare and encrypt added

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const bcrypt = require('bcrypt-nodejs');
+const passwordStrength = require('../src/api/verify/verify.strings').strength;
 
 const expect = chai.expect;
 chai.use(chaiHttp);
@@ -79,6 +81,113 @@ describe('API', function() {
                 expect(res).to.have.nested.property('body.value');
                 expect(secondRes).to.have.nested.property('body.value');
                 expect(res.body.value).to.not.equal(secondRes.body.value);
+            });
+        });
+
+        describe('encrypt', function() {
+            it('should return a valid encrypted value for a given password', async function() {
+                const password = 'mypassword';
+                const res = await chai.request(server)
+                    .get(url.ENCRYPT_ENDPOINT)
+                    .query({ value: password });
+
+                expect(res).to.have.nested.property('body.hashedValue');
+                bcrypt.compare(password, res.body.hashedValue, (err, isMatch) => {
+                    if(err) {
+                        throw err;
+                    }
+
+                    expect(isMatch).to.be.true;
+                });
+            });
+        });
+
+        describe('compare', function() {
+            it('should return true if the hash value is achievable from that password', async function() {
+                const password = 'mypassword';
+                const hashValue = '$2a$10$ZikP06bCR6GTiTs1uEx4c.zNNFtgkHXj6MWDn.XeU20me4RIEQZai';
+
+                const res = await chai.request(server)
+                    .get(url.COMPARE_ENDPOINT)
+                    .query({ hash: hashValue, value: password });
+
+                expect(res).to.have.nested.property('body.result');
+                expect(res.body.result).to.be.true;
+            });
+
+            it('should return false if the hash value is not achievable from that password', async function() {
+                const password = 'anotherverydifferentpassword';
+                const hashValue = '$2a$10$ZikP06bCR6GTiTs1uEx4c.zNNFtgkHXj6MWDn.XeU20me4RIEQZai';
+
+                const res = await chai.request(server)
+                    .get(url.COMPARE_ENDPOINT)
+                    .query({ hash: hashValue, value: password });
+
+                expect(res).to.have.nested.property('body.result');
+                expect(res.body.result).to.be.false;
+            });
+        });
+
+        describe('verify', function() {
+            it('should return weak if the password is shorter than 10 characters', async function() {
+                const password = 'Sh0rt!';
+
+                const res = await chai.request(server)
+                    .get(url.VERIFY_ENDPOINT)
+                    .query({ value: password });
+
+console.log(res.body)
+                expect(res.body.result).that.equals(passwordStrength.WEAK);
+            });
+
+            it('should return weak if it doesn\'t have at least one letter in lowercase', async function() {
+                const password = 'MYP4S5W0RD!';
+
+                const res = await chai.request(server)
+                    .get(url.VERIFY_ENDPOINT)
+                    .query({ value: password });
+
+                expect(res.body.result).that.equals(passwordStrength.WEAK);
+            });
+
+            it('should return weak if it doesn\'t have at least one letter in uppercase', async function() {
+                const password = 'myp4s5w0rd!';
+
+                const res = await chai.request(server)
+                    .get(url.VERIFY_ENDPOINT)
+                    .query({ value: password });
+
+                expect(res.body.result).that.equals(passwordStrength.WEAK);
+            });
+
+            it('should return weak if it doesn\'t have at least one number', async function() {
+                const password = 'MyPassword!';
+
+                const res = await chai.request(server)
+                    .get(url.VERIFY_ENDPOINT)
+                    .query({ value: password });
+
+                expect(res.body.result).that.equals(passwordStrength.WEAK);
+            });
+
+            it('should return weak if it doesn\'t have at least one symbol', async function() {
+                const password = 'MyP4s5w0rd';
+
+                const res = await chai.request(server)
+                    .get(url.VERIFY_ENDPOINT)
+                    .query({ value: password });
+
+                expect(res.body.result).that.equals(passwordStrength.WEAK);
+            });
+
+            it('should return strong if the password meets every condition', async function() {
+                const password = 'MyP4s5w0rd!';
+
+                const res = await chai.request(server)
+                    .get(url.VERIFY_ENDPOINT)
+                    .query({ value: password });
+
+                expect(res.body.result).that.equals(passwordStrength.STRONG);
             });
         });
     });
